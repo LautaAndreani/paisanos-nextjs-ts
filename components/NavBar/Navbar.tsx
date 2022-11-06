e57fb72ef1304e07d8b94035d84bf31cd6ad5da3
Lautaro Andreani <lautaro.andreani@gmail.com>
2022-11-06 11:59:33 -0300

    feat: rigth column main movie

--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -1,42 +1,42 @@
 import Image from "next/image"
 
 export default function NavBar() {
-	return (
-		<header className="fixed top-0 w-full bg-transparent">
-			<nav className="flex justify-between items-center p-6 text-white">
-				<div className="flex items-center gap-10">
-					<h1 className="text-4xl font-bold">PaisaFlix</h1>
-					<div className="left-links">
-						<ul className="flex gap-8">
-							<li>
-								<a href="/">Home</a>
-							</li>
-							<li>
-								<a href="/">Contact Us</a>
-							</li>
-							<li>
-								<a href="/">FAQ</a>
-							</li>
-							<li>
-								<a href="/">Pricing</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div className="flex gap-8 items-center">
-					<figure>
-						<button>
-							<Image src="/icons/search.svg" alt="search icon" width="20" height="20" />
-						</button>
-					</figure>
-					<figure>
-						<button>
-							<Image src="/avatars/user.png" alt="avatar profile" width="40" height="40" />
-						</button>
-					</figure>
-					<p className="font-bold">Soy Paisanx</p>
-				</div>
-			</nav>
-		</header>
-	)
+  return (
+    <header className="fixed top-0 w-full bg-transparent z-10 backdrop-blur-sm">
+      <nav className="flex justify-between items-center p-6 text-white">
+        <div className="flex items-center gap-10">
+          <h1 className="text-4xl font-bold">PaisaFlix</h1>
+          <div className="left-links">
+            <ul className="flex gap-8">
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">Contact Us</a>
+              </li>
+              <li>
+                <a href="/">FAQ</a>
+              </li>
+              <li>
+                <a href="/">Pricing</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex gap-8 items-center">
+          <figure>
+            <button>
+              <Image src="/icons/search.svg" alt="search icon" width="20" height="20" />
+            </button>
+          </figure>
+          <figure>
+            <button>
+              <Image src="/avatars/user.png" alt="avatar profile" width="40" height="40" />
+            </button>
+          </figure>
+          <p className="font-bold">Soy Paisanx</p>
+        </div>
+      </nav>
+    </header>
+  )
 }
